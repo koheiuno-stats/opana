266a2b9f53ae6e112f9a0f21fc436853b0c8218b
koheiuno-stats <koheiuno-stats@users.noreply.github.com>
2022-11-25 18:11:07 +0900

    angle plot

--- R/joint_plot.R
+++ R/joint_plot.R
@@ -10,19 +10,22 @@
 joint_plot <- function(data_array, file_name=NULL, joint_name=NULL, xyplot="xy"){
     if(xyplot=="xy"){
         x = data_array[,joint_name,"X"]
+        n = length(x)
         y = data_array[,joint_name,"Y"]
         df = data.frame(x=x, y=y)
     }
     if(xyplot=="x"){
         x = data_array[,joint_name,"X"]
+        n = length(x)
         df = data.frame(x=x)        
     }
     if(xyplot=="y"){
         y = data_array[,joint_name,"Y"]
+        n = length(y)
         df = data.frame(y=y)
     }    
     df = reshape2::melt(df)
-    df = cbind(df, rep(1:nrow(df),ncol(df)-1))
+    df = cbind(df, rep(1:n,ncol(df)-1))
     colnames(df) = c("axis", "value", "time")
     gg = ggplot2::ggplot(df, ggplot2::aes(x=time, y=value, color=axis)) + ggplot2::geom_line() + ggsci::scale_color_nejm()
     gg = gg + ggplot2::ggtitle(file_name)
