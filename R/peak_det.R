0432398db960f46a139762533be6f21cb72fc108
koheiuno-stats <koheiuno-stats@users.noreply.github.com>
2022-11-29 22:39:29 +0900

    delete zoo

--- R/peak_det.R
+++ R/peak_det.R
@@ -12,8 +12,9 @@ peak_det <- function(y, w=1,span=0.05){
   n = length(y)
   x = seq_along(y)
   y.smooth <- stats::loess(y ~ x, span=span)$fitted
-  y.max <- zoo::rollapply(y.smooth, 2*w+1, max, align="center")
+  y.max <- y.smooth ##zoo::rollapply(y.smooth, 2*w+1, max, align="center")
   delta <- y.max - y.smooth[-c(1:w, n+1-1:w)]
   i.max <- which(delta <= 0) + w
   return(i.max)
 }
+
