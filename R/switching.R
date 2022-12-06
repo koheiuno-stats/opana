3c8c0072ba6ee0353c9195c7abad71b1b4a77fd7
koheiuno-stats <koheiuno-stats@users.noreply.github.com>
2022-12-06 17:35:28 +0900

    new switching

--- R/switching.R
+++ R/switching.R
@@ -17,9 +17,9 @@ switching <- function(epoch_list){
     
     for(i in seq_along(epoch_list)){
         for(j in 1:5){
-            for(t in 2:(dim(epoch_list[[i]])[1])){
-                dist1 = mean((epoch_list[[i]][t, joints[[j]][1], 1:2] - new_list[[i]][t-1, joints[[j]][1], 1:2])^2, na.rm=TRUE)
-                dist2 = mean((epoch_list[[i]][t, joints[[j]][2], 1:2] - new_list[[i]][t-1, joints[[j]][1], 1:2])^2, na.rm=TRUE)
+            for(t in 3:(dim(epoch_list[[i]])[1])){
+                dist1 = mean((epoch_list[[i]][t, joints[[j]][1], 1:2] - mean(new_list[[i]][(t-2):(t-1), joints[[j]][1], 1:2]))^2)
+                dist2 = mean((epoch_list[[i]][t, joints[[j]][2], 1:2] - mean(new_list[[i]][(t-2):(t-1), joints[[j]][1], 1:2]))^2)
                 if(dist2 < dist1){
                     new_list[[i]][t, joints[[j]][1], 1:2] = epoch_list[[i]][t, joints[[j]][2], 1:2]
                     new_list[[i]][t, joints[[j]][2], 1:2] = epoch_list[[i]][t, joints[[j]][1], 1:2]
