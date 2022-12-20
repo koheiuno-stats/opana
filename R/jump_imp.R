48d7c06bf5fd91b9ca76204688663df1b147096b
koheiuno-stats <koheiuno-stats@users.noreply.github.com>
2022-12-21 03:13:12 +0900

    jump_imp

--- R/jump_imp.R
+++ R/jump_imp.R
@@ -26,26 +26,30 @@ jump_imp <- function(epoch_list, sd_ratio=1, lag=3, fit=0.2, except_joint=NULL){
             id_y = c(1:length(Y))[c(rep(0,lag),abs(diff_y)) >= sd_diffy*sd_ratio]
         
             X[id_x] = NA
-            Y[id_y] = NA            
-
+            Y[id_y] = NA
             
-            x_d = y_d = NULL
+##            x_d = y_d = NULL
             if(sum(is.na(X)) <= length(X)*0.8){
                 new_X = zoo::na.approx(X, na.rm=FALSE)
-                x_d = c(1:length(X))[abs(new_X - epoch_list[[i]][, j, 1]) > fit]            
+                sp = imputeTS::na.interpolation(X, option="spline")
+##                x_d = c(1:length(X))[abs(new_X - epoch_list[[i]][, j, 1]) > fit]            
             }
             if(sum(is.na(Y)) <= length(Y)*0.8){
                 new_Y = zoo::na.approx(Y, na.rm=FALSE)
-                y_d = c(1:length(Y))[abs(new_Y - epoch_list[[i]][, j, 2]) > fit]
+                sp = imputeTS::na.interpolation(Y, option="spline")                
+##                y_d = c(1:length(Y))[abs(new_Y - epoch_list[[i]][, j, 2]) > fit]
             }
 
-            epoch_list[[i]][union(x_d, y_d), j, 1:2] = NA 
-            if(sum(is.na(epoch_list[[i]][, j, "X"])) > length(X)*0.8){
-               epoch_list[[i]][, j, 1:2] = NA
-                next
-            }
-            epoch_list[[i]][, j, "X"] = zoo::na.approx(epoch_list[[i]][, j, "X"], na.rm = FALSE)
-            epoch_list[[i]][, j, "Y"] = zoo::na.approx(epoch_list[[i]][, j, "Y"], na.rm = FALSE)
+##            epoch_list[[i]][union(x_d, y_d), j, 1:2] = NA 
+##            if(sum(is.na(epoch_list[[i]][, j, "X"])) > length(X)*0.8){
+##               epoch_list[[i]][, j, 1:2] = NA
+##                next
+##            }
+##            epoch_list[[i]][, j, "X"] = zoo::na.approx(epoch_list[[i]][, j, "X"], na.rm = FALSE)
+##            epoch_list[[i]][, j, "Y"] = zoo::na.approx(epoch_list[[i]][, j, "Y"], na.rm = FALSE)
+
+            epoch_list[[i]][, j, "X"] = new_X
+            epoch_list[[i]][, j, "Y"] = new_Y
         }
     }
     return(epoch_list)
