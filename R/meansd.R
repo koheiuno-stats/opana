3956ab5f32b513b84ff95567c425619490f5d089
koheiuno-stats <koheiuno-stats@users.noreply.github.com>
2023-03-20 20:34:43 +0900

    meansd

--- R/meansd.R
+++ R/meansd.R
@@ -1,4 +1,11 @@
-
+#' @title meansd
+#'
+#' @description meansd() can convert epoch list to data array. 
+#'
+#' @param epoch_list epoch list.
+#' @param Jlist joint list.
+#'
+#' @export
 
 meansd <- function(epoch_list, Jlist){
     for(j in seq_along(Jlist)){
@@ -6,7 +13,6 @@ meansd <- function(epoch_list, Jlist){
         R = paste0("R", Jlist[j])
 
         diff_LX = diff_LY = diff_RX = diff_RY 
-        
-
+    }
     
 }
