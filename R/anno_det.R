a990b3f9a184eaf60c6877a17e7d7a6deb84b413
koheiuno-stats <koheiuno-stats@users.noreply.github.com>
2023-05-17 19:35:45 +0900

    traj

--- R/anno_det.R
+++ R/anno_det.R
@@ -13,6 +13,7 @@
 anno_det <- function(epoch_list, original_list, percentile = 95){
     new_list = epoch_list
     DIM = dim(epoch_list[[1]])
+
     for(j in 1:(DIM[2])){
         X = Y = rep(0, DIM[1])
         for(t in 1:DIM[1]){
@@ -23,8 +24,14 @@ anno_det <- function(epoch_list, original_list, percentile = 95){
             }
             X[t] = median(x, na.rm=TRUE)
             Y[t] = median(y, na.rm=TRUE)            
-        }        
-        for(i in 1:length(epoch_list)){
+        }
+        original_j = numeric(0)        
+        for(i in seq_along(new_list)){
+            z1 = zoo::na.approx(new_list[[i]][, j, 1], na.rm=FALSE)
+            new_list[[i]][ ,j, 1] = z1            
+            z2 = zoo::na.approx(new_list[[i]][, j, 2], na.rm=FALSE)
+            new_list[[i]][ ,j, 2] = z2            
+            
             wc_x_j = epoch_list[[i]][ , j, 1] - X
             wc_y_j = epoch_list[[i]][ , j, 2] - Y
 
@@ -34,15 +41,18 @@ anno_det <- function(epoch_list, original_list, percentile = 95){
             id_y = c(1:length(y_Z))[abs(y_Z) > abs(qnorm((100 - percentile)/200)) | is.na(y_Z)]
 
             original_list[[i]][union(id_x, id_y), j, 1:2] = NA
+            original_j = rbind(original_j, original_list[[i]][, j, 1:2])
+        }
+        
+        z1 = zoo::na.approx(original_j[,1], na.rm=FALSE)
+        z2 = zoo::na.approx(original_j[,2], na.rm=FALSE)        
 
-            z1 = zoo::na.approx(original_list[[i]][, j, 1], na.rm=FALSE)
-            original_list[[i]][ ,j, 1] = z1            
-            z2 = zoo::na.approx(original_list[[i]][, j, 2], na.rm=FALSE)
-            original_list[[i]][ ,j, 2] = z2            
+        for(i in seq_along(new_list)){
+            original_list[[i]][ , j, 1] = z1[((i-1)*DIM[1]+1):(i*DIM[1])]
+            original_list[[i]][ , j, 2] = z2[((i-1)*DIM[1]+1):(i*DIM[1])]            
         }        
     }
     return(list(original=original_list, new=new_list))
 }
-
 
 
--- R/trajectory_color.R
+++ R/trajectory_color.R
@@ -10,16 +10,24 @@
 
 trajectory_color <- function(coord_epoch, emg_epoch, muscle_name=NULL, joint_name=NULL, title_name=NULL){
     X_list = Y_list = E_list = list()
-    X_mean = Y_mean = E_mean = rep(0,dim(coord_epoch[[1]])[1])
+
     for(i in seq_along(coord_epoch)){
-        n = dim(coord_epoch[[i]])[1]
-        X_list[[i]] = coord_epoch[[i]][, joint_name, "X"]
-        Y_list[[i]] = coord_epoch[[i]][, joint_name, "Y"]
+        X_list[[i]] = coord_epoch[[i]][, joint_name, 1]
+        Y_list[[i]] = coord_epoch[[i]][, joint_name, 2]
         E_list[[i]] = emg_epoch[[i]][, muscle_name]
-        
-        X_mean = X_mean + X_list[[i]]*(1/length(coord_epoch))
-        Y_mean = Y_mean + Y_list[[i]]*(1/length(coord_epoch))
-        E_mean = E_mean + E_list[[i]]*(1/length(coord_epoch))
+    }
+
+    X_mean = Y_mean = E_mean = rep(0,(dim(coord_epoch[[1]])[1]))    
+    for(t in 1:(dim(coord_epoch[[1]])[1])){
+        X_mean_list = Y_mean_list = E_mean_list = rep(0,length(coord_epoch))
+        for(i in seq_along(coord_epoch)){
+            X_mean_list[i] = X_list[[i]][t]
+            Y_mean_list[i] = Y_list[[i]][t]
+            E_mean_list[i] = E_list[[i]][t]
+        }
+        X_mean[t] = mean(X_mean_list, na.rm=TRUE)
+        Y_mean[t] = mean(Y_mean_list, na.rm=TRUE)
+        E_mean[t] = mean(E_mean_list, na.rm=TRUE)
     }
     
     X = unlist(lapply(X_list,rbind))
@@ -34,5 +42,5 @@ trajectory_color <- function(coord_epoch, emg_epoch, muscle_name=NULL, joint_nam
     gg = ggplot2::ggplot(df, ggplot2::aes(x=x, y=y, color=emg)) + ggplot2::geom_path(lwd=1.5) + scale_color_gradientn(colors = pal)
     gg = gg + ggplot2::ggtitle(title_name)
     
-    list(mean=gg, all=gg_all)
+    list(mean=gg, all=gg_all, X_mean=X_mean, Y_mean=Y_mean, X=X, Y=Y, X_list=X_list, Y_list=Y_list)
 }
