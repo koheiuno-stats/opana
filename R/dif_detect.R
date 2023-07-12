f2bfe2c525443eb2d9e60486437d73698e743e45
koheiuno-stats <koheiuno-stats@users.noreply.github.com>
2023-07-12 17:20:10 +0900

    delete some functions

---
+++ R/dif_detect.R
@@ -0,0 +1,52 @@
+#' @title dif_detect
+#'
+#' @description dif_detect() can detect anomaly data. 
+#'
+#'
+#' @param data_array data array.
+#' @param width width is a parameter.
+#' @param scaling scaling is a scaling parameter. 
+#'
+#' @export
+
+
+dif_detect <- function(data_array, width=15, scaling=50){
+    new_array = data_array
+
+    DIM = dim(data_array)
+    for(j in 1:DIM[2]){
+        for(k in 1:2){
+            new_array[, j, k] = anom_det(data_array[, j, k], width=width, scaling=scaling)
+        }
+    }
+    return(new_array)
+}
+
+anom_det <- function(vec, width=15, scaling=50){
+    new_vec = vec
+    
+    mat = cbind(scale(seq_along(vec)), scale(vec))
+    mat[,1] = mat[,1]*scaling
+    mat[is.na(mat[,2]),1] = NA
+
+    flag = rep(0, nrow(mat))
+
+    for(i in 1:(nrow(mat)-1)){
+        if(is.na(mat[1,2])){next}
+        if(flag[i] == 1){
+            new_vec[i] = NA
+            next
+        }
+        maxi = i + width
+        if(maxi > nrow(mat)){maxi = nrow(mat)}
+        mat_diff = mat[(i+1):maxi, ] - matrix(1, length(i:maxi)-1, ncol=1)%*%mat[i,]
+        dist = apply(mat_diff, 1, function(x){mean(x^2, na.rm=TRUE)})
+        dist[dist == 0] = Inf
+        dmin = which.min(dist)
+
+        if(dmin != 1){
+            flag[(i+1):(i+dmin-1)] = 1
+        }
+    }
+    return(new_vec)
+}
--- R/miss_imp.R
+++ R/miss_imp.R
@@ -5,15 +5,11 @@
 #'
 #' @param data_array data array.
 #' @param thr the threshold parameter about confident score. 
-#' @param lag lag parameter (used in diff()).
-#' @param percentile percentile.
 #'
 #' @export
 
-miss_imp <- function(data_array, thr=0, lag=1, percentile=95){
+miss_imp <- function(data_array, thr=0){
     data_array = NA_score(data_array, thr=thr)
-    data_array = NA_diff2(data_array, lag=lag, percentile)
-
     for(j in 1:(dim(data_array)[2])){
         id_x = sum(is.na(data_array[,j,"X"])) > ((dim(data_array)[1])*0.8)
         id_y = sum(is.na(data_array[,j,"Y"])) > ((dim(data_array)[1])*0.8)
@@ -29,46 +25,6 @@ NA_score <- function(data_array, thr=0){
     for(j in 1:(dim(data_array)[2])){
         score_thr = data_array[, j, "score"] <= thr
         data_array[score_thr, j, 1:2] = NA
-    }
-    return(data_array)
-}
-
-NA_diff <- function(data_array, lag=1, percentile= 95){
-    for(j in 1:(dim(data_array)[2])){
-        x_Diff = abs(diff(data_array[,j,1], lag=lag, na.rm = TRUE))
-        x_Z = (x_Diff - mean(x_Diff, na.rm = TRUE))/sd(x_Diff, na.rm = TRUE)
-        id_x = c(1:length(x_Z))[abs(x_Z) > abs(qnorm((100 - percentile)/200))]                    
-
-        y_Diff = abs(diff(data_array[,j,2], lag=lag, na.rm = TRUE))
-        y_Z = (y_Diff - mean(y_Diff, na.rm = TRUE))/sd(y_Diff, na.rm = TRUE)
-        id_y = c(1:length(y_Z))[abs(y_Z) > abs(qnorm((100 - percentile)/200))]
-
-        data_array[union(id_x+1, id_y+1), j, 1:2] = NA
-    }
-    return(data_array)
-}
-
-NA_diff2 <- function(data_array, lag=1, percentile= 95){
-    for(j in 1:(dim(data_array)[2])){
-        vec = data_array[ ,j, 1]
-        idx = is.na(vec)
-        vec_na = vec[!idx]
-        x_Diff = abs(diff(vec_na, lag=lag, na.rm=TRUE))
-        x_Z = (x_Diff - mean(x_Diff, na.rm = TRUE))/sd(x_Diff, na.rm = TRUE)
-        vec2 = rep(NA, length(vec))
-        vec2[!idx] = c(rep(0,lag),x_Z)
-        id_x = c(1:length(vec2))[abs(vec2) > abs(qnorm((100-percentile)/200))]        
-
-        vec = data_array[ ,j, 2]
-        idx = is.na(vec)
-        vec_na = vec[!idx]
-        y_Diff = abs(diff(vec_na, lag=lag, na.rm=TRUE))
-        y_Z = (y_Diff - mean(y_Diff, na.rm = TRUE))/sd(y_Diff, na.rm = TRUE)
-        vec2 = rep(NA, length(vec))        
-        vec2[!idx] = c(rep(0,lag),y_Z)
-        id_y = c(1:length(vec2))[abs(vec2) > abs(qnorm((100-percentile)/200))]        
-
-        data_array[union(id_x+1, id_y+1), j, 1:2] = NA
     }
     return(data_array)
 }
