17f0b9e5faad4a051ad12524f9bac89a06668f9b
koheiuno-stats <koheiuno-stats@users.noreply.github.com>
2024-07-29 15:14:54 +0900

    ankle angle and hip angle change

--- R/ankle_angle.R
+++ R/ankle_angle.R
@@ -4,6 +4,7 @@
 #'
 #' @param data_array data array.
 #' @param stand bool.
+#' @param position "SmallToe" or "BigToe" or Both. 
 #'
 #' @export
 
@@ -12,47 +13,47 @@ ankle_angle <- function(data_array, stand=TRUE, LR="Left", position="SmallToe"){
 
     for(t in 1:(dim(data_array)[1])){
         K2A = data_array[t, "LAnkle", 1:2] - data_array[t, "LKnee", 1:2]
-        H2T = apply(data_array[t, c("LBigToe","LSmallToe"), 1:2], 2, mean,na.rm=TRUE) - data_array[t, "LHeel", 1:2]
+        if(is.na(K2A[1])){next}        
+        H2T = apply(data_array[t, paste0("L",position), 1:2], 2, mean,na.rm=TRUE) -
+            data_array[t, "LHeel", 1:2]        
         if(is.na(H2T[1])){next}
-
+        
         mat = rbind(data_array[t, "LAnkle", 1:2], data_array[t, "LKnee", 1:2])
         
         if(LR=="Left"){
             rK2A = rot(mat, -90)}
         if(LR=="Right"){
-            rK2A = rot(mat, 90)}
-        
+            rK2A = rot(mat, 90)}        
         H90 = c(rK2A[1,] - rK2A[2,])
         if(is.na(H2T[2] | is.na(H90[2]))){
             next
         }
-        if(H2T[2] > H90[2]){
+        out_pro = H2T[1]*K2A[2] - H2T[2]*K2A[1]            
+        if(out_pro >= 0){
             LA_angles[t] = angle(H90, H2T)            
         }else{
             LA_angles[t] = -angle(H90, H2T)                        
         }
 
-
         K2A = data_array[t, "RAnkle", 1:2] - data_array[t, "RKnee", 1:2] 
         if(is.na(K2A[1])){next}       
-        m = matrix(c(data_array[t, "RKnee", 1:2], data_array[t, "RAnkle", 1:2]),2,2, byrow = TRUE)
-
-        K2A = data_array[t, "RAnkle", 1:2] - data_array[t, "RKnee", 1:2]
-        H2T = apply(data_array[t, c("RBigToe","RSmallToe"), 1:2], 2, mean,na.rm=TRUE) - data_array[t, "RHeel", 1:2]
+        
+        H2T = apply(data_array[t, paste0("R", position), 1:2], 2, mean,na.rm=TRUE) -
+            data_array[t, "RHeel", 1:2]
         if(is.na(H2T[1])){next}
-
+    
         mat = rbind(data_array[t, "RAnkle", 1:2], data_array[t, "RKnee", 1:2])
 
         if(LR=="Left"){
             rK2A = rot(mat, -90)}
         if(LR=="Right"){
             rK2A = rot(mat, 90)}
-        H90 = c(rK2A[1,] - rK2A[2,])
-        
+        H90 = c(rK2A[1,] - rK2A[2,])        
         if(is.na(H2T[2] | is.na(H90[2]))){
             next
         }        
-        if(H2T[2] > H90[2]){
+        out_pro = H2T[1]*K2A[2] - H2T[2]*K2A[1]            
+        if(out_pro >= 0){
             RA_angles[t] = angle(H90, H2T)            
         }else{
             RA_angles[t] = -angle(H90, H2T)                        
--- R/hip_angle.R
+++ R/hip_angle.R
@@ -3,39 +3,28 @@
 #' @description hip_angle() estimates hip angles based on some. 
 #'
 #' @param data_array data array.
-#' @param stand bool.
 #' @export
 #' 
-hip_angle <- function(data_array, stand=TRUE){
-    H2floor = H2K = c(0,2)
+hip_angle <- function(data_array){
+    N2H = H2K = c(0,2)
     LH_angles = RH_angles = matrix(0,nrow=dim(data_array)[1],ncol=1)
     for(t in 1:(dim(data_array)[1])){
-        H2floor[1] = 0
-        if(stand == TRUE){
-            H2floor[2] = min(data_array[t,,"Y"], na.rm=TRUE) - data_array[t, "LHip", "Y"] 
+        N2H = data_array[t, "MidHip", 1:2] - data_array[t, "Neck", 1:2]
+        H2K = data_array[t, "LKnee", 1:2] - data_array[t, "MidHip", 1:2]
+
+        if(N2H[1] > H2K[1]){
+            LH_angles[t] = angle(N2H, H2K)
         }else{
-            H2floor[2] = max(data_array[t,,"Y"], na.rm=TRUE) - data_array[t, "LHip", "Y"] 
-        }
-        H2K = data_array[t, "LKnee", 1:2] - data_array[t, "LHip", 1:2]
-        if(is.na(H2K[1])){next}
-        if(H2K[1] > 0){
-            LH_angles[t] = angle(H2floor, H2K)
-        }else{
-            LH_angles[t] = -angle(H2floor, H2K)            
+            LH_angles[t] = -angle(N2H, H2K)            
         }
 
-        H2floor[1] = 0
-        if(stand == TRUE){
-            H2floor[2] = min(data_array[t,,"Y"], na.rm=TRUE) - data_array[t, "RHip", "Y"]
+        N2H = data_array[t, "MidHip", 1:2] - data_array[t, "Neck", 1:2]
+        H2K = data_array[t, "RKnee", 1:2] - data_array[t, "MidHip", 1:2]
+
+        if(N2H[1] > H2K[1]){
+            RH_angles[t] = angle(N2H, H2K)
         }else{
-            H2floor[2] = max(data_array[t,,"Y"], na.rm=TRUE) - data_array[t, "RHip", "Y"] 
-        }
-        H2K = data_array[t, "RKnee", 1:2] - data_array[t, "RHip", 1:2]
-        if(is.na(H2K[1])){next}        
-        if(H2K[1] > 0){
-            RH_angles[t] = angle(H2floor, H2K)
-        }else{
-            RH_angles[t] = -angle(H2floor, H2K)
+            RH_angles[t] = -angle(N2H, H2K)
         }
     }
     list(Left=LH_angles ,Right=RH_angles)
